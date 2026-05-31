a9cf9cdb76f755493bd91d1d61d5f585ed272a88
tibor-is-back <tibor.kantor@icloud.com>
2026-05-31 23:31:48 +0200

    chore(places) - separate constants, acceibility identifier files

---
+++ places/Places/Core/Constants+Common.swift
@@ -0,0 +1,10 @@
+// swiftlint:disable nesting
+
+extension Constants {
+    enum Common {
+        enum Strings {
+            static let invalidNumber = "Enter a valid number"
+            static let outOfRange = "Value must be between %@ and %@"
+        }
+    }
+}
