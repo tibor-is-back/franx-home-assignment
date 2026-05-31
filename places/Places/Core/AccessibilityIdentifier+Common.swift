51af8daacef6edd05bd0c8e1e103ba46618a67c6
tibor-is-back <tibor.kantor@icloud.com>
2026-05-31 23:26:27 +0200

    feature(places) - chore accessibility identifiers

---
+++ places/Places/Core/AccessibilityIdentifier+Common.swift
@@ -0,0 +1,12 @@
+// swiftlint:disable nesting
+
+extension AccessibilityIdentifier {
+    enum Common {
+        static let loading = "common_loading"
+        static let error = "common_error"
+        static let errorRetry = "common_error_retry"
+        static let toast = "common_toast"
+        static let toastMessage = "common_toast_message"
+        static let toastDismiss = "common_toast_dismiss"
+    }
+}
